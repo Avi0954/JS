941229471165dbad11d686ed4af646334519fb3a
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-05-17 15:18:08 +0000

    operations

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,26 @@
+let value = 10
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+  
+
+let str1 = "op"
+let str2 = "ko"
+let str3= str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+let gameCounter = 10
+++gameCounter;
+console.log(gameCounter);  //prefix me value phle implement ho jti hai or postfix me use hone ke baad 
