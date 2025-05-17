8635dcec981a8f4b72c02634c6a3ba6f5dfc0756
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-05-17 11:20:38 +0000

    datatypes

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "okok@gmail.com"
+var accountPassword = "23456"
+accountCity = "Etawa"
+
+
+let accountState;
+
+
+accountEmail = "opop@gmail.com"
+accountPassword = "87654"
+accountCity = "Jabalpur"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
