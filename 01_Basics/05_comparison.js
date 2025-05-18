1a8514dfe4849b7178ba78dc4f7d5a816ed9d279
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-05-18 09:29:48 +0000

    comparison

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,22 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1 );
+console.log( 2 ==1);
+console.log( 2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0 );
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0 );   // undefined hamesha false dega
+
+// ===  value ke datatypes bhi check krega
+
+
+console.log( "2" === 2 );
