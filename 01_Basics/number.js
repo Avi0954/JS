1e5d9985cf4ee7251ab5f5e80b6f4395fc49a9ec
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-06-04 18:57:01 +0000

    Numbers

---
+++ 01_Basics/number.js
@@ -0,0 +1,18 @@
+//Number
+
+const score = 10
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const newnum = 29.7868
+console.log(newnum.toPrecision(1));
+
+const hun = 10000000
+console.log(hun.toLocaleString('en-IN'));  
+
+
+
