42d00680fce3c2dcdad1b42a3f3329e88110a6b0
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-06-05 16:31:15 +0000

    maths

---
+++ 01_Basics/09_maths.js
@@ -0,0 +1,17 @@
+console.log(Math);
+console.log(Math.abs(-4)); //value positive cnvrt dega
+console.log(Math.round(4.9));//Nearer roundoff dega-ceil(high value lega),floor(low value lega)
+console.log(Math.ceil(5.3));//6
+console.log(Math.floor(5.3));//5
+console.log(Math.min(2,6,8,9));//2
+console.log(Math.max(9,34,4,3));//34
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+//Repeat
