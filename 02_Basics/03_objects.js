3e7fbf61591340c82d08e9b89b75625688c3ec21
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-06-08 17:37:59 +0000

    Object basics

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,6 +1,45 @@
-//Singleton
+//OBJECT - can be declare (literals) (constructor[Singleton])   
+//Jb constructor se object declare krenge singleton bnega vice versa literals
+
+
+
 
 //object literals
+
+const Sym = Symbol("key")
+
 const User = {
-    name: "Avi"
+    name: "Avi",
+    age: 17,
+    [Sym]: "mykey",
+    day: "Monday",
+    isLoggedIn: false,
+   
+    
+
+
 }
+
+console.log(User.name)
+console.log(User["name"])
+console.log(User[Sym]) // [ ] type 
+
+User.age = 20// to change
+// Object.freeze(User)// phir koi changes na ho to isse freeze krdenge (Lock krna)
+// console.log(User);
+
+
+// To declrare a function
+
+User.Benten = function(){
+    console.log("Its hero time");
+}
+
+User.Gwen = function(){
+    console.log('Magic Power, ${this.age}'); //jb same object ko refrence krna ho  (check this line of code NP)
+
+} 
+
+
+console.log(User.Benten());
+console.log(User.Gwen());
