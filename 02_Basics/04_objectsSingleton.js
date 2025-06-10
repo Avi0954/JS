350f7fe031b548ca601b7b79454cd89bc41f6aa4
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-06-10 06:55:58 +0000

    singleton

--- 02_Basics/04_objectsSingleton.js
+++ 02_Basics/04_objectsSingleton.js
@@ -21,3 +21,32 @@ const App = {
 
 console.log(App.name.fullname.firstname.lastname);
 
+const obj1 = {1: "BB", 2:"DARK"}
+const obj2 = {3: "ST", 4:"FROM"}
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const user = [
+    {
+        series: 1,
+        email: "DARK"
+    },
+    {
+        series: 1,
+        email: "DARK"
+    },
+    {
+        series: 1,
+        email: "DARK"
+    },
+    {
+        series: 1,
+        email: "DARK"
+    },
+]
+
+user[1].email
+console.log(BreakingBad);
+console.log(Object.keys(BreakingBad));  //****** 
+console.log(Object.values(BreakingBad));  
+console.log(Object.entries(BreakingBad));    
