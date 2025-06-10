6e5e3bd3020647d5f389dd71b5a8cdb5ab69eeae
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-06-10 07:50:30 +0000

    Destructure and JSON API

---
+++ 02_Basics/05_deStuc_JSONAPI.js
@@ -0,0 +1,22 @@
+//deStructure
+const OTT = {
+    netflix: "DARK",
+    price: 199,
+    season: 3
+
+}
+const {season: episode} = OTT //destructure
+//console.log(season);
+console.log(episode); //output same dega
+
+
+
+
+//JSON API
+
+
+// {
+//     "name": "mindhunter",
+//     "OTT": "netflix",
+//     "price": 199
+// }
