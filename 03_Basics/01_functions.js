56c2bc7e9b44e3f62dabc216d26d84d5cfd91182
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-06-10 08:03:26 +0000

    resume

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,11 @@
+function yourname() {
+    console.log("M");
+    console.log("I");
+    console.log("T");
+    console.log("S");
+    console.log("U");
+    console.log("H");
+    console.log("A");
+}
+
+yourname()
