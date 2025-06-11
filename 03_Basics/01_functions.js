ee14c5c624e80ac933c6dfbac7b91bf7a7a4d8c6
Avinash <143734562+Avi0954@users.noreply.github.com>
2025-06-11 10:17:20 +0000

    Function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,3 +1,4 @@
+//Function and Parameter
 function yourname() {
     console.log("M");
     console.log("I");
@@ -9,3 +10,42 @@ function yourname() {
 }
 
 yourname()
+
+
+//adding
+//M1
+function addTwoNum(num1, num2){
+    console.log(num1+ num2);
+}
+
+addTwoNum(5, 5)
+addTwoNum(5, null)
+addTwoNum(5, "5")
+
+//fun ko jb define krate hai uske andar ki jo value hoti usko bolte hai parameter 
+//or jb call karate hai toh argument
+
+
+
+//M2
+function addTwoNum(num1, num2){
+    let result = num1 + num2
+    return result
+}
+const result = addTwoNum(5,5)
+console.log("Result: ",result);
+
+
+
+//M3
+function addTwoNum(num1, num2){
+    return num1 + num2 
+}
+const resul = addTwoNum(5,5)
+console.log("Result: ",result);
+
+
+
+
+
+//Parameter
